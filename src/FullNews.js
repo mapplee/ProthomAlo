bd8f0d1cb9ae987ecb2652833424e8491ec64b05
Wasiur Rahman <osiur2@gmail.com>
2022-09-14 11:39:23 +0600

    Add Link on JibonJapon

--- src/FullNews.js
+++ src/FullNews.js
@@ -1,17 +1,17 @@
-import news from "./data";
+import japon from "./data";
 import { useParams } from "react-router-dom";
 
 function FullNews (){
     const {id} = useParams();
-    const s_news = news[id];
-    const {heading, image, ptime} = news[id];
+    const s_news = japon[id];
+    const {heading, image, ptime} = japon[id];
     return (
         
         <div className="w-7/12 h-4/6 m-10 flex justify-center">
             <div className="w-7/12 h-4/6 ">
             <p className="text-4xl font-bold">{heading}</p>
             <img src={s_news.image} className="w-400 h-300 "/>
-            <p>{news[id].ptime}</p>
+            <p>{japon[id].ptime}</p>
 
             </div>
         </div>
--- src/JibonJapon.js
+++ src/JibonJapon.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+import japon from "./data";
 
-function Jibon ({image, heading, news, ptime}){
+function Jibon ({image, heading, news, ptime, id}){
+    
     return (
-            <div className="w-[25%] h-full p-5 ">
+        <Link to = {"/" + id} className="w-[25%] h-full p-5">
+            
                         
                 <img src = {image} className="w-full h-full"/>
                 <p className="text-2xl font-bold mt-2">{heading}</p>                       
@@ -9,20 +13,12 @@ function Jibon ({image, heading, news, ptime}){
                 <p className="mt-1 text-gray-600">{ptime}</p>
 
                        
-            </div>
+            </Link>
     );
 }
 
-const japon = [
-            {image: "/prothom-alo-images/1479.webp",    heading: "এমির লালগালিচা মাতানো ১০ ফ্যাশন", news: "লালগালিচার সবচেয়ে পরিচিত পোশাক আরমানির ফ্লোরলেন্থ গোলাপি গাউন তো ছিলই, এর বাইরেও দেখা গেল লুই ভুঁতোর স্কয়ার কাট ক্রপ টপ বা", ptime: "৫ ঘণ্টা আগে"},
-            {image: "/prothom-alo-images/THUMB.webp",   heading: "ঢাকায় সালমান খানের ফ্যাশন ব্র্যান্ড",   news: "সালমান খান বলেন, ‘হাই বাংলাদেশ। ঢাকার প্রাণকেন্দ্রে আপনাদের জন্য চমক নিয়ে আসছে বিয়িং হিউম্যান ক্লদিং লাইন।", ptime: "৫ ঘণ্টা আগে"},
-            {image: "/prothom-alo-images/AB_1436.webp", heading: "ভাদ্রে কেন কাপড় রোদে দেবেন?",     news: "শখের পোশাক সামান্য রোদ না পাওয়ার কারণে নষ্ট হয়ে যেতে পারে। ভাদ্রের রোদের প্রবল তাপে আলমারিতে অনেক দিন আটকে থাকা কাপড়", ptime: "৫ ঘণ্টা আগে"},
-            {image: "/prothom-alo-images/SY_4749.webp", heading: "অ্যাপল জিঞ্জার মোহিতোর রেসিপি",    news: "আপেল ও আদার রসে তৈরী হবে এই পানীয়...", ptime: "৫ ঘণ্টা আগে"}
-            ];
-
-
 function JibonJapon (){
-    const jibon_japon = japon.map(t => <Jibon image ={t.image} heading ={t.heading} news = {t.news} ptime = {t.ptime}/>);
+    const jibon_japon = japon.map((t, i) => <Jibon id={i} image ={t.image} heading ={t.heading} news = {t.news} ptime = {t.ptime}/>);
     return (
         
         <div className="w-full h-full mt-1 ">
